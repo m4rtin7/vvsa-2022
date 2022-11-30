89fdd0a26f5144ef9b599615487657d95d124f4b
Adam Gonšenica <adagon001@gmail.com>
2022-11-30 23:14:03 +0100

    stupid unit test

---
+++ WebApplication1/unit-testing-using-dotnet-test/TransactionService.Tests/TransactionService_UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace TransactionService.Tests;
+using WebApplication1.Service;
+using backend.DatabaseModel;
+
+public class UnitTest1
+{
+    [Fact]
+    public void IsPrime_InputIs1_ReturnFalse()
+    {
+        //var primeService = new TransactionService();
+        //var tr = new Transaction();
+
+        //TransactionViewModel result = primeService.ToTransactionViewModel(tr);
+        Assert.False(false, "1 should not be prime");
+
+        //Assert.Equal(tr, result);
+    }
+}
