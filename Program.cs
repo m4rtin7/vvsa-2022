37334de39fca1053851a71bdc8f947b2319dfb58
m4rtin7 <m.zavadzan@gmail.com>
2022-12-07 17:39:15 +0100

    heroku

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using backend.DatabaseModel;
+using Microsoft.Extensions.FileProviders;
+using WebApplication1.Repository;
+using WebApplication1.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<whiyes5oContext>();
+builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+builder.Services.AddScoped<ITransactionService, TransactionService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "frontend/dist/frontend"))
+});
+
+app.MapControllers();
+
+app.Run();
